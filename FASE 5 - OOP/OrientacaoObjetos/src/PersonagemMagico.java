3815125dbf5ae3ee916f314f9bc8dd8bcc04313a
henriquegheno <euhenriquegheno@gmail.com>
2024-08-21 21:44:40 -0300

    oop

---
+++ FASE 5 - OOP/OrientacaoObjetos/src/PersonagemMagico.java
@@ -0,0 +1,14 @@
+public class PersonagemMagico {
+    //atributos
+    String nome;
+    int nivelEnergia;
+    String poderMagico;
+
+    public PersonagemMagico() {
+
+    }
+
+    public PersonagemMagico(String nome) {
+        this.nome = nome;
+    }
+}
